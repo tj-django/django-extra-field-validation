b4fdb0a35decc98d0930be6412178619218bc132
Tonye Jack <tonye@coherentpath.com>
2018-12-08 16:01:48 -0500

    Updated manage.py

--- manage.py
+++ manage.py
@@ -7,9 +7,16 @@ if __name__ == "__main__":
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+        if sys.version_info > (2, 7):
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            ) from exc
+        else:
+            raise  ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
     execute_from_command_line(sys.argv)
