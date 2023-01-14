fad2bbcc2cacff95268f07ffa222c84592b868e0
Tonye Jack <jtonye@ymail.com>
2023-01-14 00:34:00 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ else:
 
 setup(
     name="django-extra-field-validation",
-    version="1.1.2",
+    version="1.2.2",
     description="Extra django field validation.",
     python_requires=">=2.6",
     long_description=LONG_DESCRIPTION,
